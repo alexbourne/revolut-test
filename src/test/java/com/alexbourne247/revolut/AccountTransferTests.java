2857bf7d2eefd77bab974668057f98f63d8ba8d0
Alex Bourne <alexbourne247@gmail.com>
2018-03-14 22:31:58 +0000

    Add balance check in tests

--- src/test/java/com/alexbourne247/revolut/AccountTransferTests.java
+++ src/test/java/com/alexbourne247/revolut/AccountTransferTests.java
@@ -8,12 +8,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import ratpack.test.MainClassApplicationUnderTest;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4.class)
 public class AccountTransferTests {
@@ -47,11 +45,14 @@ public class AccountTransferTests {
     @Test
     public void successfulTransferWithSufficientFunds() throws Exception {
         assertEquals(TransferStatus.TRANSFERRED, mapper.readValue(post("transfer", "{ \"fromAccountId\": 12345, \"toAccountId\": 23456, \"amount\": 150.99 }"), TransferStatus.class));
-    }
+        assertEquals(getBalance(12345), 49.01, 0.01);
+        assertEquals(getBalance(23456), 150.99, 0.01);    }
 
     @Test
     public void unsuccessfulTransferDueToInsufficientFunds() throws Exception {
         assertEquals(TransferStatus.INSUFFICIENT_FUNDS, mapper.readValue(post("transfer", "{ \"fromAccountId\": 12345, \"toAccountId\": 23456, \"amount\": 2000.00 }"), TransferStatus.class));
+        assertEquals(getBalance(12345), 200.0, 0.01);
+        assertEquals(getBalance(23456), 0.0, 0.01);
     }
 
     @Test
@@ -91,6 +92,15 @@ public class AccountTransferTests {
         }
     }
 
+    private static double getBalance(int accountId) throws SQLException {
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery("select balance from accounts where accountId = " + accountId);
+            if (rs.next()) {
+                return rs.getDouble(1);
+            }
+        }
 
+        return Double.NaN;
+    }
 
 }
